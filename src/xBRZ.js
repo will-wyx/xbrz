e5a6217114196c5dbbd2488dfbdef20dddbfb19b
will-wyx <wyx_msn@hotmail.com>
2020-04-29 16:02:56 +0800

    feat(xBRZ): 支持6倍放大

--- src/xBRZ.js
+++ src/xBRZ.js
@@ -38,7 +38,7 @@ class BlendResult {
 }
 
 const maxRots = 4;
-const maxScale = 5;
+const maxScale = 6;
 const maxScaleSq = maxScale * maxScale;
 
 class OutputMatrix {
@@ -64,7 +64,93 @@ class OutputMatrix {
     }
 }
 
-class Scaler {
+class Scaler6x {
+    constructor() {
+        this.scale = 6;
+    }
+
+    scale() {
+        return this.scale
+    }
+
+    blendLineShallow(col, out) {
+        alphaBlend(1, 4, out.ref(this.scale - 1, 0), col);
+        alphaBlend(1, 4, out.ref(this.scale - 2, 2), col);
+        alphaBlend(1, 4, out.ref(this.scale - 3, 4), col);
+
+        alphaBlend(3, 4, out.ref(this.scale - 1, 1), col);
+        alphaBlend(3, 4, out.ref(this.scale - 2, 3), col);
+        alphaBlend(3, 4, out.ref(this.scale - 3, 5), col);
+
+        out.ref(this.scale - 1, 2).set(col);
+        out.ref(this.scale - 1, 3).set(col);
+        out.ref(this.scale - 1, 4).set(col);
+        out.ref(this.scale - 1, 5).set(col);
+
+        out.ref(this.scale - 2, 4).set(col);
+        out.ref(this.scale - 2, 5).set(col);
+    }
+
+    blendLineSteep(col, out) {
+        alphaBlend(1, 4, out.ref(0, this.scale - 1), col);
+        alphaBlend(1, 4, out.ref(2, this.scale - 2), col);
+        alphaBlend(1, 4, out.ref(4, this.scale - 3), col);
+
+        alphaBlend(3, 4, out.ref(1, this.scale - 1), col);
+        alphaBlend(3, 4, out.ref(3, this.scale - 2), col);
+        alphaBlend(3, 4, out.ref(5, this.scale - 3), col);
+
+        out.ref(2, this.scale - 1).set(col);
+        out.ref(3, this.scale - 1).set(col);
+        out.ref(4, this.scale - 1).set(col);
+        out.ref(5, this.scale - 1).set(col);
+
+        out.ref(4, this.scale - 2).set(col);
+        out.ref(5, this.scale - 2).set(col);
+    }
+
+    blendLineSteepAndShallow(col, out) {
+        alphaBlend(1, 4, out.ref(0, this.scale - 1), col);
+        alphaBlend(1, 4, out.ref(2, this.scale - 2), col);
+        alphaBlend(3, 4, out.ref(1, this.scale - 1), col);
+        alphaBlend(3, 4, out.ref(3, this.scale - 2), col);
+
+        alphaBlend(1, 4, out.ref(this.scale - 1, 0), col);
+        alphaBlend(1, 4, out.ref(this.scale - 2, 2), col);
+        alphaBlend(3, 4, out.ref(this.scale - 1, 1), col);
+        alphaBlend(3, 4, out.ref(this.scale - 2, 3), col);
+        out.ref(2, this.scale - 1).set(col);
+        out.ref(3, this.scale - 1).set(col);
+        out.ref(4, this.scale - 1).set(col);
+        out.ref(5, this.scale - 1).set(col);
+
+        out.ref(4, this.scale - 2).set(col);
+        out.ref(5, this.scale - 2).set(col);
+
+        out.ref(this.scale - 1, 2).set(col);
+        out.ref(this.scale - 1, 3).set(col);
+    }
+
+    blendLineDiagonal(col, out) {
+        alphaBlend(1, 2, out.ref(this.scale - 1, this.scale / 2), col);
+        alphaBlend(1, 2, out.ref(this.scale - 2, this.scale / 2 + 1), col);
+        alphaBlend(1, 2, out.ref(this.scale - 3, this.scale / 2 + 2), col);
+
+        out.ref(this.scale - 2, this.scale - 1).set(col);
+        out.ref(this.scale - 1, this.scale - 1).set(col);
+        out.ref(this.scale - 1, this.scale - 2).set(col)
+    }
+
+    blendCorner(col, out) {
+        alphaBlend(97, 100, out.ref(5, 5), col);
+        alphaBlend(42, 100, out.ref(4, 5), col);
+        alphaBlend(42, 100, out.ref(5, 4), col);
+        alphaBlend(6, 100, out.ref(5, 3), col);
+        alphaBlend(6, 100, out.ref(3, 5), col);
+    }
+}
+
+class Scaler5x {
     constructor() {
         this.scale = 5;
     }
@@ -418,9 +504,7 @@ function scalePixel(scaler, rotDeg, ker3x3, trg, trgi, trgWidth, blendInfo) {
     }
 }
 
-const scaleSize = 5;
-
-export function scaleImage(src, trg, srcWidth, srcHeight, yFirst, yLast) {
+export function scaleImage(scaleSize, src, trg, srcWidth, srcHeight, yFirst, yLast) {
     yFirst = Math.max(yFirst, 0);
     yLast = Math.min(yLast, srcHeight);
 
@@ -547,7 +631,18 @@ export function scaleImage(src, trg, srcWidth, srcHeight, yFirst, yLast) {
             ker3[h] = src[s_p1 + x];
             ker3[i] = src[s_p1 + x_p1];
 
-            const scaler = new Scaler();
+            let scaler;
+            switch (scaleSize) {
+                case 5:
+                    scaler = new Scaler5x();
+                    break;
+                case 6:
+                    scaler = new Scaler6x();
+                    break;
+                default:
+                    scaler = new Scaler6x();
+                    break;
+            }
 
             scalePixel(scaler, 0, ker3, trg, trgi, trgWidth, blend_xy);
             scalePixel(scaler, 1, ker3, trg, trgi, trgWidth, blend_xy);
