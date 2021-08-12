5570cf9ed67b336268399ef4ac09b84c49260682
will-wyx <wyx_msn@hotmail.com>
2021-08-12 16:41:56 +0800

    style(*): 代码整理

--- src/scalers/Scaler3x.js
+++ src/scalers/Scaler3x.js
@@ -1,49 +1,49 @@
-import {alphaBlend} from "./Blender.js";
+import { alphaBlend } from './Blender.js'
 
 export default class Scaler3x {
-    constructor() {
-        this.scale = 3;
-    }
+  constructor () {
+    this.scale = 3
+  }
 
-    scale() {
-        return this.scale
-    }
+  scale () {
+    return this.scale
+  }
 
-    blendLineShallow(col, out) {
-        alphaBlend(1, 4, out.ref(this.scale - 1, 0), col);
-        alphaBlend(1, 4, out.ref(this.scale - 2, 2), col);
+  blendLineShallow (col, out) {
+    alphaBlend(1, 4, out.ref(this.scale - 1, 0), col)
+    alphaBlend(1, 4, out.ref(this.scale - 2, 2), col)
 
-        alphaBlend(3, 4, out.ref(this.scale - 1, 1), col);
+    alphaBlend(3, 4, out.ref(this.scale - 1, 1), col)
 
-        out.ref(this.scale - 1, 2).set(col);
-    }
+    out.ref(this.scale - 1, 2).set(col)
+  }
 
-    blendLineSteep(col, out) {
-        alphaBlend(1, 4, out.ref(0, this.scale - 1), col);
-        alphaBlend(1, 4, out.ref(2, this.scale - 2), col);
+  blendLineSteep (col, out) {
+    alphaBlend(1, 4, out.ref(0, this.scale - 1), col)
+    alphaBlend(1, 4, out.ref(2, this.scale - 2), col)
 
-        alphaBlend(3, 4, out.ref(1, this.scale - 1), col);
+    alphaBlend(3, 4, out.ref(1, this.scale - 1), col)
 
-        out.ref(2, this.scale - 1).set(col);
-    }
+    out.ref(2, this.scale - 1).set(col)
+  }
 
-    blendLineSteepAndShallow(col, out) {
-        alphaBlend(1, 4, out.ref(2, 0), col);
-        alphaBlend(1, 4, out.ref(0, 2), col);
+  blendLineSteepAndShallow (col, out) {
+    alphaBlend(1, 4, out.ref(2, 0), col)
+    alphaBlend(1, 4, out.ref(0, 2), col)
 
-        alphaBlend(3, 4, out.ref(2, 1), col);
-        alphaBlend(3, 4, out.ref(1, 2), col);
+    alphaBlend(3, 4, out.ref(2, 1), col)
+    alphaBlend(3, 4, out.ref(1, 2), col)
 
-        out.ref(2, 2).set(col);
-    }
+    out.ref(2, 2).set(col)
+  }
 
-    blendLineDiagonal(col, out) {
-        alphaBlend(1, 8, out.ref(1, 2), col);
-        alphaBlend(1, 8, out.ref(2, 1), col);
-        alphaBlend(7, 8, out.ref(2, 2), col);
-    }
+  blendLineDiagonal (col, out) {
+    alphaBlend(1, 8, out.ref(1, 2), col)
+    alphaBlend(1, 8, out.ref(2, 1), col)
+    alphaBlend(7, 8, out.ref(2, 2), col)
+  }
 
-    blendCorner(col, out) {
-        alphaBlend(45, 100, out.ref(2, 2), col);
-    }
+  blendCorner (col, out) {
+    alphaBlend(45, 100, out.ref(2, 2), col)
+  }
 }
